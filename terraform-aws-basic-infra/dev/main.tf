fbec71a30ec5cfe6009b00ee08545b22131e3aae
Jake Furlong <jake.w.furlong@gmail.com>
2022-05-17 16:36:06 -0600

    added tf lock file to gitignore and updated main

--- terraform-aws-basic-infra/dev/main.tf
+++ terraform-aws-basic-infra/dev/main.tf
@@ -8,3 +8,17 @@ terraform {
     dynamodb_table = "ndo-dev-terraform-locks"
   }
 }
+
+module "network" {
+  source = "github.com/jakefurlong/terraform/modules/network"
+
+  vpc_cidr              = "172.16.0.0/16"
+  env                   = "Development"
+  public-subnet-cidr-a  = "172.16.0.0/24"
+  public-subnet-cidr-b  = "172.16.1.0/24"
+  public-subnet-cidr-c  = "172.16.2.0/24"
+  private-subnet-cidr-a = "172.16.3.0/24"
+  private-subnet-cidr-b = "172.16.4.0/24"
+  private-subnet-cidr-c = "172.16.5.0/24"
+  my-ip                 = "174.51.57.8"
+}
