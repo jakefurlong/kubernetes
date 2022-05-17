76df99f535c167de26aceac8981c72312aec1f33
Jake Furlong <jake.w.furlong@gmail.com>
2022-05-17 16:51:15 -0600

    data file for latest AL2 AMI id

---
+++ terraform-aws-basic-infra/dev/data.tf
@@ -0,0 +1,20 @@
+# Return the AMI id of the most current Amazon Linux 2 Machine Image
+data "aws_ami" "std_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
